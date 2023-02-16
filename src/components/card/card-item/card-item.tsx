67ca0beede1132848d63d6fca8b007dfc77ce65b
gpesantos <gpesantos@recordtv.com.br>
2023-02-16 00:39:07 -0300

    Ordenando de forma correta

--- src/components/card/card-item/card-item.tsx
+++ src/components/card/card-item/card-item.tsx
@@ -13,39 +13,53 @@ export type ItemFazendaProps = {
 } 
 
 export function CardItem({ fazenda }: { fazenda: ItemFazendaProps[] }) {
-  var count = 1;  
+  
+  function test (votos, total) {
+    var calc = ((votos*100) / total).toFixed(0);
+    return calc
+  }
+
+  // array temporário 
+  const order = fazenda.map(function({ __id, name, description, positive, picture, negative }) {
+    return { __id, name, description, positive, picture, negative, value: positive };
+  })
+
+  // ordenando o array 
+  order.sort(function(a, b) {
+    return +(a.value < b.value) || +(a.value === b.value) - 1;
+  });
+
+
+    const result = order.map(function(el, index){
+    const numbersPositive = el.positive || 0 ;
+    const numbersNegative = el.negative || 0;
+    const total = numbersPositive + numbersNegative; 
+
+    return (
+      <Card key={el.__id}>
+        <div className='flex items-end flex-row-reverse'>
+          <Image className="card-item-image relative rounded-full h-[50px] border-[#b93739] border-4 border-solid lg:w-20 lg:h-20" src={el.picture} title={`Fotografia de ${el.name}`} alt={`Fotografia de ${el.name}`} width={50} height={50} loading='lazy'/>
+          <Text as="span" color="tertiary" className="absolute rounded-full font-bold border-2 border-tertiary w-6 h-6 items-center bg-neutral-400 justify-center flex">
+            {index +1}
+          </Text>
+        </div>
+        <div className='ml-4'>
+          <Text as="p" color="primary" className='card-item-name font-bold text-lg'>
+            {el.name}
+          </Text>
+          <Text as="p" color="secondary" className='card-item-description font-semi'>
+            {el.description.replace("&ordm;", "°")}
+          </Text>
+        </div>
+        <Tooltip porcentagemPositive={`${test(numbersPositive, total)}%`} porcentagemNegative={`${test(numbersNegative, total)}%`} />
+      </Card>
+    );
+  });
 
   return (
     <>
-      {[...fazenda]
-        .sort((a, b) => b.negative - a.positive )
-        .map(fazenda => {
-        var numbersPositive = fazenda.positive || 0 ;
-        var numbersNegative = fazenda.negative || 0;
-        var total = numbersPositive + numbersNegative; 
-        var percentagePositive = ((numbersPositive*100) / total).toFixed(0);
-        var percentageNegative = ((numbersNegative*100) / total).toFixed(0);
-          
-        return (
-          <Card key={fazenda.__id}>
-             <div className='flex items-end flex-row-reverse'>
-               <Image className="card-item-image relative rounded-full h-[50px] border-[#b93739] border-4 border-solid lg:w-20 lg:h-20" src={fazenda.picture} title={`Fotografia de ${fazenda.name}`} alt={`Fotografia de ${fazenda.name}`} width={50} height={50} loading='lazy'/>
-               <Text as="span" color="tertiary" className="absolute rounded-full font-bold border-2 border-tertiary w-6 h-6 items-center bg-neutral-400 justify-center flex">
-                 {count++}
-               </Text>
-             </div>
-             <div className='ml-4'>
-               <Text as="p" color="primary" className='card-item-name font-bold text-lg'>
-                 {fazenda.name}
-               </Text>
-               <Text as="p" color="secondary" className='card-item-description font-semi'>
-                 {fazenda.description.replace("&ordm;", "°")}
-               </Text>
-             </div>
-             <Tooltip porcentagemPositive={`${percentagePositive}%`} porcentagemNegative={`${percentageNegative}%`} />
-          </Card>
-        );
-      })}
+      {result}
     </>
-  );
+
+  )
 }
