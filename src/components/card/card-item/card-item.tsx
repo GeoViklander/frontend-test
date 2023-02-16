4fc2389ee220b8d0d406948dedfd78ac4f05f2e8
gpesantos <gpesantos@recordtv.com.br>
2023-02-16 13:11:33 -0300

    Ajustes

--- src/components/card/card-item/card-item.tsx
+++ src/components/card/card-item/card-item.tsx
@@ -30,25 +30,25 @@ export function CardItem({ fazenda }: { fazenda: ItemFazendaProps[] }) {
   });
 
 
-    const result = order.map(function(el, index){
-    const numbersPositive = el.positive || 0 ;
-    const numbersNegative = el.negative || 0;
+    const result = order.map(function(item, index){
+    const numbersPositive = item.positive || 0 ;
+    const numbersNegative = item.negative || 0;
     const total = numbersPositive + numbersNegative; 
 
     return (
-      <Card key={el.__id}>
+      <Card key={item.__id}>
         <div className='flex items-end flex-row-reverse'>
-          <Image className="card-item-image relative rounded-full h-[50px] border-[#b93739] border-4 border-solid lg:w-20 lg:h-20" src={el.picture} title={`Fotografia de ${el.name}`} alt={`Fotografia de ${el.name}`} width={50} height={50} loading='lazy'/>
+          <Image className="card-item-image relative rounded-full h-[50px] border-[#b93739] border-4 border-solid lg:w-20 lg:h-20" src={item.picture} title={`Fotografia de ${item.name}`} alt={`Fotografia de ${item.name}`} width={50} height={50} loading='lazy'/>
           <Text as="span" color="tertiary" className="absolute rounded-full font-bold border-2 border-tertiary w-6 h-6 items-center bg-neutral-400 justify-center flex">
             {index +1}
           </Text>
         </div>
         <div className='ml-4'>
           <Text as="p" color="primary" className='card-item-name font-bold text-lg'>
-            {el.name}
+            {item.name}
           </Text>
           <Text as="p" color="secondary" className='card-item-description font-semi'>
-            {el.description.replace("&ordm;", "°")}
+            {item.description.replace("&ordm;", "°")}
           </Text>
         </div>
         <Tooltip porcentagemPositive={`${test(numbersPositive, total)}%`} porcentagemNegative={`${test(numbersNegative, total)}%`} />
